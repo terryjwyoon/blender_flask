6b6ff1e250fb314576d62ae03ad75999f9220138
TY-MacBook <terryjwyoon@gmail.com>
2022-08-19 15:19:40 +0900

    add: custom python function call test

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,20 @@ from flask import Flask
 from flask import render_template
 from flask import request, jsonify
 
+from static.python.test1 import testFunction
+
 app = Flask(__name__)
 
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
 @app.route('/TY_test1', methods=['GET', 'POST'])
 def TY_test1():
     if request.form.get('registration') == 'success':
@@ -15,6 +23,14 @@ def TY_test1():
 
     return jsonify({'abc': 'registration unsuccessful'})
 
+@app.route('/TY_test2', methods=['GET', 'POST'])
+def TY_test2():
+    b = testFunction()
+    return b
+
+#==============================================================================
+#
+#==============================================================================
 if __name__ == '__main__':
     # 127.0.0.1:9000
     app.run('0.0.0.0', port=9000, debug=True)
---
+++ static/python/test1.py
@@ -0,0 +1,3 @@
+def testFunction():
+    a = "success"
+    return a
